e1ae3820b8a93c5195150d10ac49f4923d0d08e4
Kelvin <yifeng.liang@accenture.com>
2016-11-17 11:17:47 +0800

    fast commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp'),
     path = require('path'),
+    inject = require('gulp-inject'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify-css'),
+    gulpif = require('gulp-if'),
+    rev = require('gulp-rev'),
     del = require('del'),
     nodemon = require("gulp-nodemon"),
     uglify = require('gulp-uglify'),
     html = require('gulp-htmlmin'),
     bowerfile = require('main-bower-files'),
+    less = require('gulp-less'),
     settings = {
         removeComments: true,
         collapseWhitespace: true,
@@ -16,37 +22,60 @@ var gulp = require('gulp'),
         minifyCSS: true
     };
 
+var ENV = process.argv[process.argv.length - 1] == 'serve' ? 'serve' : 'dist',
+    ROOT = 'resources';
+
 gulp.task('clean', function (cb) {
-    return del(path.join('dist'), cb);
+    return del([path.join('serve', 'images', '*'), path.join('serve', '*'), path.join('dist', 'images', '*'), path.join('dist', '*')], cb);
 });
 gulp.task('lib', function () {
     return gulp.src(bowerfile())
-        .pipe(uglify())
-        .pipe(gulp.dest(path.join('dist', 'lib')));
+        .pipe(gulpif(ENV == 'dist', uglify()))
+        .pipe(gulp.dest(path.join(ENV, 'lib')));
 });
 gulp.task('config', function () {
-    return gulp.src(path.join('resources', 'config', '*'))
-        .pipe(gulp.dest(path.join('dist', 'config')));
+    return gulp.src(path.join(ROOT, 'config', '*'))
+        .pipe(gulp.dest(path.join(ENV, 'config')));
 });
 gulp.task('module', function () {
-    return gulp.src(path.join('resources', 'module', '*'))
-        .pipe(uglify())
-        .pipe(gulp.dest(path.join('dist', 'module')));
+    return gulp.src(path.join(ROOT, 'module', '*'))
+        .pipe(gulpif(ENV == 'dist', uglify()))
+        .pipe(gulp.dest(path.join(ENV, 'module')));
 });
 gulp.task('html', function () {
-    return gulp.src(path.join('resources', 'views', '*'))
-        .pipe(html(settings))
-        .pipe(gulp.dest(path.join('dist', 'views')));
+    return gulp.src(path.join(ROOT, 'views', '*'))
+        .pipe(gulpif(ENV == 'dist', html(settings)))
+        .pipe(gulp.dest(path.join(ENV, 'views')));
 });
-gulp.task('default', ['clean', 'lib', 'config', 'module', 'html'], function () {
-    return gulp.src(path.join('resources', 'index.html'))
-        .pipe(html(settings))
-        .pipe(gulp.dest(path.join('dist')));
+gulp.task('image', function () {
+    return gulp.src(path.join(ROOT, 'images', '**'))
+        .pipe(gulp.dest(path.join(ENV, 'images')));
+});
+gulp.task('less', function () {
+    return gulp.src(path.join(ROOT, 'stylesheets', '**.less'))
+        .pipe(less())
+        .pipe(gulpif(ENV == 'dist', concat('pc.css')))
+        .pipe(gulpif(ENV == 'dist', minify()))
+        .pipe(gulpif(ENV == 'dist', rev()))
+        .pipe(gulp.dest(path.join(ENV, 'css')));
+});
+gulp.task('index', function () {
+    return gulp.src(path.join(ROOT, 'index.html'))
+        .pipe(gulp.dest(path.join(ENV)))
+});
+gulp.task('default', ['clean', 'image', 'lib', 'config', 'module', 'less', 'html', 'index'], function () {
+    return gulp.src(path.join(ENV, 'index.html'))
+        .pipe(inject(gulp.src([path.join(ENV, 'css', '*.css')], {read: false}), {relative: true}))
+        .pipe(gulpif(ENV == 'dist', html(settings)))
+        .pipe(gulp.dest(path.join(ENV)))
 });
 gulp.task('serve', ['default'], function () {
-    gulp.watch(path.join('resources', 'module', '**.js'), ['module']);
-    gulp.watch(path.join('resources', 'views', '**.html'), ['html']);
-    gulp.watch(path.join('resources', 'config', '**.js'), ['config']);
+    gulp.watch(path.join(ROOT, 'images', '**'), ['image']);
+    gulp.watch(path.join(ROOT, 'module', '**.js'), ['module']);
+    gulp.watch(path.join(ROOT, 'views', '**.html'), ['html']);
+    gulp.watch(path.join(ROOT, 'index.html'), ['index']);
+    gulp.watch(path.join(ROOT, 'config', '**.js'), ['config']);
+    gulp.watch(path.join(ROOT, 'stylesheets', '**.less'), ['less']);
     return nodemon({
         script: 'gulp-serve.js',
         ext: 'js html',
